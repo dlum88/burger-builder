98292972fc8c55e3d4dc20ea986844ef22d036a4
David <dlum988@gmail.com>
2021-02-06 19:08:37 -0500

    added styling to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import './Layout.css';
+// import classes from './Layout.css';
+// CSS Modules is available In newer versions of React, you do not have to import classes. 
+// Instead just import the entire file and set the className to the appropriate property from the CSS file.
+
 
 const layout = ({children}) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className="Content">
       {children}
     </main>
   </Aux>
