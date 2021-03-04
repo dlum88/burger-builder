68ffd11d575094c428b61fba0ebe44fbdc0b6ca1
David <dlum988@gmail.com>
2021-03-03 18:01:03 -0600

    created Navigation components

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from'./NavigationItem.module.css'
+console.log(classes)
+
+const navigationItem = ({ children, link, active }) => (
+  <li className={classes.NavigationItem}>
+    <a 
+    href={link}
+    className={active ? classes.active : null}
+    >{children}</a>
+  </li>
+);
+
+export default navigationItem;
