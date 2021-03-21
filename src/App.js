10815d0024daf657eee9676b851593cb1b88163d
David <dlum988@gmail.com>
2021-03-21 19:18:28 -0400

    Added routing to application. Able to navigate and render to /checkout and /burger-builder

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Switch, Route } from 'react-router-dom';
 class App extends Component {
+
+  // ?? How to grab ingredients? from params?
+  // implement Routes here to show BurgerBuilder / Checkout
+  // add NavLink to Layout navigation buttons
+  // in OrderSummary, implement Link to route to Checkout component
+  // and implement Link to route back to BurgerBuilder on cancelation
 
   render() {
     return (
       <div >
           <Layout>
-            <BurgerBuilder/>
-            <Checkout />
+            <Switch>
+              <Route path="/build-burger" component={BurgerBuilder}/>
+              <Route path="/checkout" component={Checkout} />
+            </Switch>
           </Layout>
       </div>
     );
